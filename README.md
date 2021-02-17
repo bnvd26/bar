c71dd3cf4d82947f910b336b6c6762ff36e8aa6a
Ben Ad <benadida05@gmail.com>
2021-02-17 15:37:24 +0100

    add readme

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # bar
 
+# Lancer le projet
+## Si vous avez docker sur votre machine
+Tappez la commande suivante à la racine du projet bar
+<br>
+```sh init.sh```
+<br>
+Puis rendez vous sur le http://localhost
+
+## Si vous n'avez pas docker
+Assurez que votre version de PHP est <= 7.4
+<br>
+Assurez vous que votre fichier .env est bien configuré avec votre environnement (DATABASE_URL).
+<br>
+Ensuite
+<br>
+```composer install```
+<br>
+```php bin/console doctrine:database:create```
+<br>
+```php bin/console doctrine:migrations:migrate```
+<br>
+```php bin/console doctrine:fixtures:load```
+<br>
+<br>
+```npm install && npm run dev```
+<br>
+Enfin lancez cette commande qui vas vous permettre de démarrer votre serveur :
+<br>
+```symfony server:start````
+<br>
+Enfin : [ici] : http://127.0.0.1:8000
 # Fixtures
 
 Nous avons décidez partir sur quelque chose de plus structuré pour nos Fixtures pour cela nous avons séparé de façon à ce que chaque Entités ai sa propre Fixtures ceci permet notemment une meilleure organisation pour les dependances entre chacune d'entre elles.
--- migrations/Version20210217143302.php
+++ migrations/Version20210217143302.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210215111419 extends AbstractMigration
+final class Version20210217143302 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,24 +20,32 @@ final class Version20210215111419 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE beer (id INT AUTO_INCREMENT NOT NULL, country_id_id INT DEFAULT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT NOT NULL, published_at DATETIME DEFAULT NULL, INDEX IDX_58F666ADD8A48BBD (country_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE beer (id INT AUTO_INCREMENT NOT NULL, country_id_id INT DEFAULT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT NOT NULL, published_at DATETIME DEFAULT NULL, price INT DEFAULT NULL, degree INT DEFAULT NULL, INDEX IDX_58F666ADD8A48BBD (country_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT DEFAULT NULL, term VARCHAR(100) DEFAULT \'normal\' NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category_beer (category_id INT NOT NULL, beer_id INT NOT NULL, INDEX IDX_D36D1CEA12469DE2 (category_id), INDEX IDX_D36D1CEAD0989053 (beer_id), PRIMARY KEY(category_id, beer_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, address LONGTEXT DEFAULT NULL, email VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(100) NOT NULL, weight NUMERIC(4, 1) NOT NULL, name VARCHAR(100) NOT NULL, number_beer INT NOT NULL, age INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE statistic (id INT AUTO_INCREMENT NOT NULL, beer_id INT DEFAULT NULL, customer_id INT DEFAULT NULL, score INT NOT NULL, INDEX IDX_649B469CD0989053 (beer_id), INDEX IDX_649B469C9395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE beer ADD CONSTRAINT FK_58F666ADD8A48BBD FOREIGN KEY (country_id_id) REFERENCES country (id)');
         $this->addSql('ALTER TABLE category_beer ADD CONSTRAINT FK_D36D1CEA12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE category_beer ADD CONSTRAINT FK_D36D1CEAD0989053 FOREIGN KEY (beer_id) REFERENCES beer (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE statistic ADD CONSTRAINT FK_649B469CD0989053 FOREIGN KEY (beer_id) REFERENCES beer (id)');
+        $this->addSql('ALTER TABLE statistic ADD CONSTRAINT FK_649B469C9395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE category_beer DROP FOREIGN KEY FK_D36D1CEAD0989053');
+        $this->addSql('ALTER TABLE statistic DROP FOREIGN KEY FK_649B469CD0989053');
         $this->addSql('ALTER TABLE category_beer DROP FOREIGN KEY FK_D36D1CEA12469DE2');
         $this->addSql('ALTER TABLE beer DROP FOREIGN KEY FK_58F666ADD8A48BBD');
+        $this->addSql('ALTER TABLE statistic DROP FOREIGN KEY FK_649B469C9395C3F3');
         $this->addSql('DROP TABLE beer');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE category_beer');
         $this->addSql('DROP TABLE country');
+        $this->addSql('DROP TABLE customer');
+        $this->addSql('DROP TABLE statistic');
     }
 }
